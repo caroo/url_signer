1d1bca250d252b02160491b6e42ff999b2a6eb3a
Andreas Riemer <andreas.riemer@gmail.com>
2013-03-11 21:40:17 +0100

    Removes activesupport dependency

--- lib/url_signer.rb
+++ lib/url_signer.rb
@@ -1,14 +1,13 @@
 require "url_signer/version"
 
-require 'active_support'
 require 'uri'
 require 'digest/sha1'
 
 class UrlSigner
-  
+
   DEFAULT_HASH_LENGTH = 10
   DEFAULT_SEPARATOR = "_"
-  
+
   # Initialize new url-signer
   #
   # == Parameter
@@ -16,18 +15,18 @@ class UrlSigner
   # options (Hash):: possible options are:
   #                 - :hash_length (Integer):: set length of hash
   #                 - :separator (String):: set separator for hash in url
-  # 
+  #
   def initialize(secret, options = {})
     @secret = secret
     @hash_length = options[:hash_length] || DEFAULT_HASH_LENGTH
     @separator = options[:separator] || DEFAULT_SEPARATOR
   end
-  
+
   # signs a given url
-  # 
+  #
   # == Parameter
   # url (String):: whole URL to sign
-  # 
+  #
   # == Returns
   # signed URL (url with inserted hash)
   #
@@ -36,12 +35,12 @@ class UrlSigner
     suffix = File.extname(parsed_url.path)
     parsed_url.to_s.gsub(/#{suffix}$/, "#{hash_with_separator(url)}#{suffix}")
   end
-  
+
   # get url without hash (as it was before signing)
-  # 
+  #
   # == Parameter
   # url (String):: url with inserted hash
-  # 
+  #
   # == Returns
   # Returns a URL without hash (as it was before signing)
   def unsign(url)
@@ -53,34 +52,34 @@ class UrlSigner
     unsigned_url = "#{url_without_hash_and_suffix}#{suffix}"
     return unsigned_url
   end
-  
+
   # validates a given URL
-  # 
+  #
   # == Parameter
   # url (String):: url to validate
-  # 
+  #
   # == Returns
   # true or false
   def valid?(url)
     hash(unsign(url)) == extract_hash(url)
   end
-  
-    
+
+
   private
-  
+
     def hash(string)
       Digest::SHA1.hexdigest(string + @secret)[0..(@hash_length - 1)]
     end
-    
+
     def extract_hash(url)
       parsed_url = URI.parse(url)
       suffix = File.extname(parsed_url.path)
       extracted_hash = File.basename(parsed_url.path,suffix)[-@hash_length..-1]
       return extracted_hash
     end
-    
+
     def hash_with_separator(url)
       "#{@separator}#{hash(url)}"
     end
-    
+
 end
--- test/url_signer_test.rb
+++ test/url_signer_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class TestUrlSigner < Test::Unit::TestCase
-  
+
   def test_should_set_length_of_hash_in_initializer
     url_signer = UrlSigner.new("secret", :hash_length => 16)
     url = "http://images.pkw.de/audi_gebrauchtwagen_300.jpg"
@@ -11,20 +11,20 @@ class TestUrlSigner < Test::Unit::TestCase
     end
     assert url_signer.valid?(url_signer.sign(url))
   end
-  
+
   def test_should_sign_urls
     url_signer = UrlSigner.new("secret")
     url = "http://images.pkw.de/audi_gebrauchtwagen_300.jpg"
     signed_url = url_signer.sign(url)
     assert_not_equal url, signed_url
   end
-  
+
   def test_round_trip
     url_signer = UrlSigner.new("secret")
     url = "http://images.pkw.de/audi_gebrauchtwagen_300.jpg"
     assert url_signer.valid?(url_signer.sign(url))
   end
-  
+
   def test_should_validate_url
     url_signer = UrlSigner.new("secret")
     signed_url = "http://images.pkw.de/audi_gebrauchtwagen_300_c1ed2a4c21.jpg"
@@ -32,7 +32,7 @@ class TestUrlSigner < Test::Unit::TestCase
     assert url_signer.valid?(signed_url)
     assert !url_signer.valid?(bad_signed_url)
   end
-  
+
   def test_should_use_secret_for_hashing
     url_signer1 = UrlSigner.new("secret1")
     url1 = "http://images.pkw.de/audi_gebrauchtwagen_300.jpg"
@@ -42,18 +42,18 @@ class TestUrlSigner < Test::Unit::TestCase
     signed_url2 = url_signer2.sign(url2)
     assert_equal false, signed_url1 == signed_url2
   end
-  
+
   def test_should_not_be_able_to_reset_secret
     url_signer = UrlSigner.new("secret")
     assert_raise(NoMethodError) { url_signer.secret = "new secret" }
   end
-  
+
   def test_should_unsign_url
     url_signer = UrlSigner.new("secret")
     url = "http://images.pkw.de/audi_gebrauchtwagen_300.jpg"
     assert_equal url, url_signer.unsign(url_signer.sign(url))
   end
-  
+
   def test_should_extract_hash_from_url
     url_signer = UrlSigner.new("secret", :hash_length => 10)
     url = "http://images.pkw.de/audi_gebrauchtwagen_300.jpg"
@@ -61,15 +61,11 @@ class TestUrlSigner < Test::Unit::TestCase
       assert_equal "c1ed2a4c21", url_signer.extract_hash(url_signer.sign(url))
     end
   end
-  
-  def test_should_have_hash_with_separator
-    url_signer = UrlSigner.new("secret")
-  end
-  
+
   def test_should_be_able_to_set_separator_in_initializer
     url_signer = UrlSigner.new("secret", :separator => "~")
     url = "http://images.pkw.de/audi_gebrauchtwagen_300.jpg"
     assert_match(/http\:\/\/images\.pkw\.de\/audi_gebrauchtwagen_300\~.{#{UrlSigner::DEFAULT_HASH_LENGTH}}\.jpg/, url_signer.sign(url))
   end
-  
+
 end
